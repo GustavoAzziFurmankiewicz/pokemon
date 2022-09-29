9d9f45974bdb144a1c8139cd2c3e638f6ad6cd61
Gustavo <gustavo.azzifurmankiewicz.st@everis.nttdata.com>
2022-09-29 11:11:25 -0300

    first commit

---
+++ lib/app_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon/app_controller.dart';
+import 'package:pokemon/login_page.dart';
+
+import 'home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: AppController.instance,
+      builder: (context, child) {
+        return MaterialApp(
+          theme: ThemeData(
+            primarySwatch: Colors.blueGrey,
+            brightness: AppController.instance.isDartTheme
+                ? Brightness.dark
+                : Brightness.light,
+          ),
+          home: LoginPage(),
+        );
+      },
+    );
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon/app_controller.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+        actions: [
+          CustomSwitch(),
+        ],
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Contador: $counter'),
+            Container(height: 10),
+            CustomSwitch(),
+            Container(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.blueGrey,
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.blueGrey,
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.blueGrey,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class CustomSwitch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+        value: AppController.instance.isDartTheme,
+        onChanged: (value) {
+          AppController.instance.changeTheme();
+        });
+  }
+}
+
+//Column terá um tamanho limite
+//Para fazer uma lista grande, utilizar Listview (Flutterando #16)
